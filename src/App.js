a5b29d57ea5a9f1ab9c89ad82fcafc8805d95b2e
esteraolszewski <estera.o@live.ca>
2022-12-03 21:08:09 -0500

    API working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect} from 'react';
 
 function App() {
+
+  useEffect( () => {
+    axios({
+      url: "https://api.api-ninjas.com/v1/exercises",
+      method: "GET",
+      contentType: "application/json",
+      headers: {'X-Api-Key': "iLcxEQg2WENDgCYYq1OUkg==9M625XLT481hht8L"},
+      params: {
+        muscle: "",
+        name: "",
+        difficulty: "",
+        offset: 1
+      }
+    }).then((res) => {
+      console.log(res);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>WORKOUT</h1>
       </header>
     </div>
   );
